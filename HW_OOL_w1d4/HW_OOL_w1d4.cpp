345fa7e1aa638ab96d6890b33ceb19f020152566
jakelord1 <jakelord1.vo@gmail.com>
2023-09-07 18:29:56 +0300

    Добавьте файлы проекта.

---
+++ HW_OOL_w1d4/HW_OOL_w1d4.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+class Abonent {
+    char* ContactName;
+    char HomeNum[11];
+    char WorkNum[11];
+    char MobilNum[11];
+    char Info[50];
+public:
+    Abonent() {
+
+    };
+};
+int main()
+{
+    cout << "Hello World!\n";
+}
