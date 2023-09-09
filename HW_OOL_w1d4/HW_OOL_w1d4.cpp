b88706670afcc390c008a920980f4794ee321884
jakelord1 <jakelord1.vo@gmail.com>
2023-09-09 19:03:32 +0300

    Just Finished

--- HW_OOL_w1d4/HW_OOL_w1d4.cpp
+++ HW_OOL_w1d4/HW_OOL_w1d4.cpp
@@ -63,8 +63,94 @@ public:
             Info[i] = ' ';
         }
     }
+    char* GetName() {
+        return ContactName;
+    }
+    char GetNum(int i) {
+        return HomeNum[i];
+    }
+    void Reseter() {
+        ContactName = nullptr;
+        for (size_t i = 0; i < 50; i++)
+        {
+            if (i < 11) {
+                HomeNum[i] = ' ';
+                WorkNum[i] = ' ';
+                MobilNum[i] = ' ';
+            }
+            Info[i] = ' ';
+        }
+    }
+    void Show(int i) {
+        cout << i+1 << " " << ContactName << "\t" << HomeNum << "\t" << WorkNum << "\t" << MobilNum << endl;
+    }
 };
+void ShowList(Abonent p[]) {
+    for (size_t i = 0; i < 15; i++)
+    {
+        p[i].Show(i);
+    }
+}
+void SearchSecName(Abonent* p) {
+    char comp[5], search[5];
+    cin.getline(search, 5);
+    for (size_t i = 0; i < 25; i++)
+    {
+        for (size_t j = 0; j < 5; j++)
+        {
+            comp[j] = p[i].GetName()[j];
+        }
+        if (strcmp(comp, search) == 0) {
+            cout << "Second Name: " << p[i].GetName() << " Name: ";
+            for (size_t i = 0; i < 11; i++)
+            {
+                cout << p->GetNum(i);
+            }
+            cout << endl;
+        }
+    }
+}
+Abonent EditAb(Abonent p) {
+    p.Enter();
+    return p;
+}
+Abonent DeleteAb(Abonent p) {
+    p.Reseter();
+    return p;
+}
 int main()
 {
     Abonent people[15];
+    int choose, pos;
+    while (1) {
+        cout << "Phone book\nMenu:\n1 - Choose from list\t2 - Edit Abonent\t3 - Delete Abonent\t4 - Search by Contact Name\t0 - Exit" << endl << "Write action: ";
+        cin >> choose;
+        switch (choose) {
+        case 1: {
+            ShowList(people);
+            cin >> pos;
+            pos--;
+            break;
+        }
+        case 2: {
+            EditAb(people[pos]);
+            break;
+        }
+        case 3: {
+            DeleteAb(people[pos]);
+            break;
+        }
+        case 4: {
+            SearchSecName(people);
+            break;
+        }
+        case 0: {
+            exit(0);
+            break;
+        }
+        default: {
+            break;
+        }
+        }
+    }
 }
